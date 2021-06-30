57f1c37c0ae1c6724ade6f7b06c2f52ea7cde9a1
Eugene Markelov <noach4se@gmail.com>
2021-07-01 02:08:49 +0300

    lesson 28 ajax added

---
+++ script.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', () => {
+    'use strict';
+
+    const select = document.getElementById('cars'),
+        output = document.getElementById('output');
+
+    async function getData() {
+        const url = './cars.json'
+        return await fetch(url)
+    }
+
+    select.addEventListener('change', () => {
+        getData()
+            .then(res => res.json())
+            .then((res) => {
+                res.cars.forEach(item => {
+                    if (item.brand === select.value) {
+                        const { brand, model, price } = item;
+                        output.innerHTML = `Тачка ${brand} ${model} <br>
+                            Цена: ${price}$`;
+                    }
+                });
+            })
+    });
+
+});
